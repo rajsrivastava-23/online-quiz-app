f5dcb49254dad9db2a3f2b1ba5aebefdea6dd63a
Utkarsh srivastava <rajsrivastava236@gmail.com>
2025-07-09 18:21:27 +0100

    fix for render deployment: port update

--- app.py
+++ app.py
@@ -33,5 +33,8 @@ def result():
 def admin():
     return render_template('admin.html')
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+import os
+
+if __name__ == "_main_":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
